268b2816054b846716aa277f66aa9d7f84ab9f1b
1PALADIN1 <sonikx94@yandex.ru>
2020-10-21 19:30:18 +0300

    Задание 1

--- Homework_1.playground/Contents.swift
+++ Homework_1.playground/Contents.swift
@@ -1,1 +1,28 @@
+import Foundation
 
+// MARK: - Task 1
+func solveQuadraticEq(a: Float, b: Float, c: Float) {
+    let d = b * b - 4 * a * c
+    
+    if d < 0 {
+        print("Корней нет!")
+        return
+    }
+    
+    if d == 0 {
+        let x = -b / 2 * a
+        print("Единственный корень: \(x)")
+        return
+    }
+    
+    let x1 = (-b + sqrt(d)) / (2 * a)
+    let x2 = (-b - sqrt(d)) / (2 * a)
+    
+    print("Корни уравнения x1 = \(x1), x2 = \(x2)")
+}
+
+solveQuadraticEq(a: 1, b: -2, c: 3)
+solveQuadraticEq(a: 1, b: -2, c: -3)
+solveQuadraticEq(a: 1, b: 12, c: 36)
+
+// MARK: - Task 2
