b14841196fc91a509b4c537e00c974c00f084d80
1PALADIN1 <sonikx94@yandex.ru>
2020-10-21 19:49:05 +0300

    Задание 2

--- Homework_1.playground/Contents.swift
+++ Homework_1.playground/Contents.swift
@@ -26,3 +26,26 @@ solveQuadraticEq(a: 1, b: -2, c: -3)
 solveQuadraticEq(a: 1, b: 12, c: 36)
 
 // MARK: - Task 2
+func findHypotenuse(cathetusA: Float, cathetusB: Float) -> Float {
+    return sqrt(cathetusA * cathetusA + cathetusB * cathetusB)
+}
+
+func findPerimeter(sideA: Float, sideB: Float, sideC: Float) -> Float {
+    return sideA + sideB + sideC
+}
+
+func findArea(cathetusA: Float, cathetusB: Float) -> Float {
+    return cathetusA * cathetusB / 2
+}
+
+let catA: Float = 12
+let catB: Float = 10
+
+let hypotenuse = findHypotenuse(cathetusA: catA, cathetusB: catB)
+print("Гипотенуза: \(hypotenuse)")
+
+let perimeter = findPerimeter(sideA: catA, sideB: catB, sideC: hypotenuse)
+print("Периметр: \(perimeter)")
+
+let area = findArea(cathetusA: catA, cathetusB: catB)
+print("Площадь: \(area)")
