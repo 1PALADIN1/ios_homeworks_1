8c967afa02a0a8f29f0e4480f0fa69c7a620538d
Ruslan Malinovskiy <sonikx94@yandex.ru>
2020-10-24 13:36:08 +0300

    Задание 2

--- Homework_2.playground/Contents.swift
+++ Homework_2.playground/Contents.swift
@@ -11,3 +11,15 @@ number = 8
 print("Число \(number) четное? \(isNumberEven(number: number))")
 number = 21
 print("Число \(number) четное? \(isNumberEven(number: number))")
+
+// MARK: - 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func isNumberDivisiblyByThree(number: Int) -> Bool {
+    return number % 3 == 0
+}
+
+number = 4
+print("Число \(number) делится на 3? \(isNumberDivisiblyByThree(number: number))")
+number = 9
+print("Число \(number) делится на 3? \(isNumberDivisiblyByThree(number: number))")
+number = 21
+print("Число \(number) делится на 3? \(isNumberDivisiblyByThree(number: number))")
